d4a3725e66e8c4b7bbddc426aa09c98484a8fd67
Aqeel <aqeel-sheikh@users.noreply.github.com>
2025-07-31 18:48:15 +0530

    Add winner check and draw condition to gameController

--- script.js
+++ script.js
@@ -62,12 +62,53 @@ function gameController() {
     console.log(`${getActivePlayer().name}'s turn.`);
   };
 
-  const playRound = (cell) => {
-    const playBoard = board.getBoard();
-    playBoard[cell].setValue(getActivePlayer().token);
+  const winPatterns = [
+    [0, 1, 2], // top row
+    [3, 4, 5], // middle row
+    [6, 7, 8], // bottom row
+    [0, 3, 6], // left column
+    [1, 4, 7], // middle column
+    [2, 5, 8], // right column
+    [0, 4, 8], // diagonal top-left to bottom-right
+    [2, 4, 6], // diagonal top-right to bottom-left
+  ];
+  const checkWinner = (board) => {
+    for (let pattern of winPatterns) {
+      const pos1val = board[pattern[0]].getValue();
+      const pos2val = board[pattern[1]].getValue();
+      const pos3val = board[pattern[2]].getValue();
 
+      if (pos1val !== null && pos1val === pos2val && pos2val === pos3val) {
+        return `${getActivePlayer().name} Won`;
+      }
+    }
+
+    if (board.every((cell) => cell.getValue() !== null)) {
+      return "Draw";
+    }
+
+    return;
+  };
+  const playRound = (cell) => {
+    const currentBoard = board.getBoard();
+
+    if (cell > currentBoard.length - 1) {
+      console.log("Invalid cell");
+      return;
+    }
+    if (currentBoard[cell].getValue() !== null) {
+      console.log("Cell already taken");
+      return;
+    }
+    
+    currentBoard[cell].setValue(getActivePlayer().token);
+    if (checkWinner(currentBoard)) {
+      board.printBoard();
+      console.log(checkWinner(currentBoard));
+      return;
+    }
     switchPlayerTurn();
-    printNewRound();
+    printNewRound()
   };
 
   printNewRound();
@@ -77,3 +118,12 @@ function gameController() {
   };
 }
 const game = gameController();
+// game.playRound(0);
+// game.playRound(1);
+// game.playRound(5);
+// game.playRound(3);
+// game.playRound(4);
+// game.playRound(2);
+// game.playRound(7);
+// game.playRound(8);
+// game.playRound(6);
