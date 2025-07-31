8d464776a72e3f1104fc27afa30588b6347ef5db
Aqeel <aqeel-sheikh@users.noreply.github.com>
2025-07-31 15:55:35 +0530

    Implement gameController to manage player turns and game flow

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function gameBoard() {
       },
     };
   }
-  
+
   function printBoard() {
     for (let i = 0; i < 3; i++) {
       let rows = board
@@ -34,3 +34,46 @@ function gameBoard() {
   };
 }
 
+function gameController() {
+  playerOneName = "Player 1";
+  playerTwoName = "Player 2";
+  const board = gameBoard();
+
+  const players = [
+    {
+      name: playerOneName,
+      token: "X",
+    },
+    {
+      name: playerTwoName,
+      token: "O",
+    },
+  ];
+
+  let activePlayer = players[0];
+
+  const switchPlayerTurn = () => {
+    activePlayer = activePlayer === players[0] ? players[1] : players[0];
+  };
+  const getActivePlayer = () => activePlayer;
+
+  const printNewRound = () => {
+    board.printBoard();
+    console.log(`${getActivePlayer().name}'s turn.`);
+  };
+
+  const playRound = (cell) => {
+    const playBoard = board.getBoard();
+    playBoard[cell].setValue(getActivePlayer().token);
+
+    switchPlayerTurn();
+    printNewRound();
+  };
+
+  printNewRound();
+
+  return {
+    playRound,
+  };
+}
+const game = gameController();
